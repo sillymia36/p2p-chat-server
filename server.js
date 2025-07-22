31dea86953e577a0477b3635394feb430fab10ce
mia <privat@miacorlito.de>
2025-07-22 11:50:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const WebSocket = require('ws');
-
 const PORT = process.env.PORT || 3000;
 const wss = new WebSocket.Server({ port: PORT });
 
@@ -7,19 +6,33 @@ const clients = new Set();
 
 wss.on('connection', (ws) => {
   clients.add(ws);
+  console.log('Client connected');
 
   ws.on('message', (message) => {
+    // Make sure the message is a string before forwarding
+    let msg = message;
+
+    if (typeof msg !== 'string') {
+      try {
+        msg = JSON.stringify(msg);
+      } catch (err) {
+        console.error('Could not stringify message:', err);
+        return;
+      }
+    }
+
+    // Relay the message to all *other* clients
     for (let client of clients) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        client.send(msg);
       }
     }
   });
 
   ws.on('close', () => {
     clients.delete(ws);
+    console.log('Client disconnected');
   });
 });
 
-console.log(`Signaling server running on port ${PORT}`);
-
+console.log(`WebSocket signaling server is running on port ${PORT}`);
